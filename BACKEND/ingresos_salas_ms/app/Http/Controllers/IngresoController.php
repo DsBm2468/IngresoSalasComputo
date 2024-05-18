228a6d06210843654b00bbc44050d9629697ae0b
DsBm2468 <dsbenavides@uniboyaca.edu.co>
2024-05-18 12:19:12 -0500

    configuración inicial

---
+++ BACKEND/ingresos_salas_ms/app/Http/Controllers/IngresoController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ingreso;
+use Illuminate\Http\Request;
+
+class IngresoController extends Controller
+{
+    function index()//Consulta general de todos los datos
+    {
+        $ingresos = Ingreso::all();//esta linea hace lo mismo que el SELECT * FROM ... todo el proceso para incluir datos a la base de datos la hace de forma más rapida
+        $data = ['data' => $ingresos];//forma de estructurar la respuesta (OPCIONAL)
+        return response()->json($data);//generar la respuesta en formato json
+    }
+
+    function show($id)//consulta especifica (un solo dato)
+    {
+        $ingreso = Ingreso::find($id);//llamas a los datos como en index pero se escribe::find()
+        $data = ['data' => $ingreso];//forma de estructurar la respuesta (OPCIONAL)
+        return response()->json($data);//generar la respuesta en formato json
+    }
+
+    function store(Request $request){//Registrar un nuevo elemento (insert into ...) 
+        $datos = $request->all();
+        $ingreso = new Ingreso();
+        $ingreso->codigoEstudiante = $datos['codigoEstudiante'];
+        $ingreso->nombreEstudiante = $datos['nombreEstudiante'];
+        $ingreso->idPrograma = $datos['idPrograma'];
+        $ingreso->fechaIngreso = $datos['fechaIngreso'];
+        $ingreso->horaIngreso = $datos['horaIngreso'];
+        $ingreso->horaSalida = $datos['horaSalida'];
+        $ingreso->idResponsableSala = $datos['idResponsable'];
+        $ingreso->idSala = $datos['idSala'];
+        $ingreso->save();
+        $data = ['data' => $ingreso];
+        return response()->json($data);
+    }
+
+    function update($id, Request $request){//hacer cambios(modificaciones)
+    //para eso, en este caso se necesita el id y el request 
+    //( permite el acceso a toda la información que pasa desde el navegador del cliente al servidor.)
+        $datos = $request->all();
+        $contacto = Ingreso::find($id);
+        $contacto->nombre = $datos['nombre'];
+        $contacto->email = $datos['email'];
+        $contacto->telefono = $datos['telefono'];
+        $contacto->save();
+        $data = ['data' => $contacto];
+        return response()->json($data);
+    }
+
+    function destroy($id)
+    {
+        $contacto = Ingreso::find($id);
+        if (empty($contacto)) {
+            $data = ['data' => 'No se encuentra registrado el contacto'];
+            return response()->json($data, 404);
+        }
+        $contacto->delete();
+        $data = ['data' => 'Datos eliminados'];
+        return response()->json($data);
+    }
+}
