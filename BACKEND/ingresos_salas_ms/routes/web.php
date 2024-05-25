0568ed17f8f0e8f6d77ba5c23b30f60e773f6219
DsBm2468 <dsbenavides@uniboyaca.edu.co>
2024-05-25 18:06:25 -0500

    creación de rutas

--- BACKEND/ingresos_salas_ms/routes/web.php
+++ BACKEND/ingresos_salas_ms/routes/web.php
@@ -22,11 +22,36 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     //EJEMPLO
     // $router->metodosHttp('/nombreTabla', 'NombreClase@nombreMetodo');
 
-    $router->get('/contactos', 'ContactoController@index');
-    $router->get('/contactos/{id}', 'ContactoController@show');
-    $router->post('/contactos', 'ContactoController@store');
-    $router->put('/contactos/{id}', 'ContactoController@update');
-    $router->delete('/contactos/{id}', 'ContactoController@destroy');
+    $router->get('/ingresos', 'IngresoController@index');
+    $router->get('/ingresos/{id}', 'IngresoController@show');
+    $router->post('/ingresos', 'IngresoController@store');
+    $router->put('/ingresos/{id}', 'IngresoController@update');
+    $router->delete('/ingresos/{id}', 'IngresoController@destroy');
+
+    $router->get('/horarios', 'HorarioController@index');
+    $router->get('/horarios/{id}', 'HorarioController@show');
+    $router->post('/horarios', 'HorarioController@store');
+    $router->put('/horarios/{id}', 'HorarioController@update');
+    $router->delete('/horarios/{id}', 'HorarioController@destroy');
+
+    $router->get('/programas', 'ProgramaController@index');
+    $router->get('/programas/{id}', 'ProgramaController@show');
+    $router->post('/programas', 'ProgramaController@store');
+    $router->put('/programas/{id}', 'ProgramaController@update');
+    $router->delete('/programas/{id}', 'ProgramaController@destroy');
+
+    $router->get('/responsables', 'ResponsableController@index');
+    $router->get('/responsables/{id}', 'ResponsableController@show');
+    $router->post('/responsables', 'ResponsableController@store');
+    $router->put('/responsables/{id}', 'ResponsableController@update');
+    $router->delete('/responsables/{id}', 'ResponsableController@destroy');
+
+    $router->get('/salas', 'SalaController@index');
+    $router->get('/salas/{id}', 'SalaController@show');
+    $router->post('/salas', 'SalaController@store');
+    $router->put('/salas/{id}', 'SalaController@update');
+    $router->delete('/salas/{id}', 'SalaController@destroy');
+    
 });
 
 
@@ -36,11 +61,11 @@ $router->group(['prefix' => 'api'], function () use ($router) {
 // PARA QUE SE PUEDA EJECUTAR LOS METODOS
 
 
-// $router->group(['prefix' => 'api'], function () use ($router) {
+
 //     $router->get('/contactos', 'ContactoController@index');
 //     $router->get('/contactos/{id}', 'ContactoController@show');
 //     $router->post('/contactos', 'ContactoController@store');
 //     $router->put('/contactos/{id}', 'ContactoController@update');
 //     $router->delete('/contactos/{id}', 'ContactoController@destroy');
-// });
+
 
