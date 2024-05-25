997408fb3053b6d38f2be72e091cf7383affe239
Dankel26 <darodrigueza@uniboyaca.edu.co>
2024-05-24 23:34:30 -0500

    se añadio notas en routes, se incluyo .env editando gitignore

--- BACKEND/ingresos_salas_ms/routes/web.php
+++ BACKEND/ingresos_salas_ms/routes/web.php
@@ -18,9 +18,29 @@ $router->get('/', function () use ($router) {
 });
 
 $router->group(['prefix' => 'api'], function () use ($router) {
+
+    //EJEMPLO
+    // $router->metodosHttp('/nombreTabla', 'NombreClase@nombreMetodo');
+
     $router->get('/contactos', 'ContactoController@index');
     $router->get('/contactos/{id}', 'ContactoController@show');
     $router->post('/contactos', 'ContactoController@store');
     $router->put('/contactos/{id}', 'ContactoController@update');
     $router->delete('/contactos/{id}', 'ContactoController@destroy');
 });
+
+
+// ROUTER SIRVE PARA LLAMAR LOS METODOS (FUNCIONES) DE LA CAPA CONTROLLER
+// CADA SECCION DE ROUTER ENTRA A UNA TABLA EN LA BASE DE DATOS
+// ES DECIR, SI TENEMOS 5 TABLAS EN LA BASE DE DATOS HACEMOS 5 SECCIONES DE ROUTER
+// PARA QUE SE PUEDA EJECUTAR LOS METODOS
+
+
+// $router->group(['prefix' => 'api'], function () use ($router) {
+//     $router->get('/contactos', 'ContactoController@index');
+//     $router->get('/contactos/{id}', 'ContactoController@show');
+//     $router->post('/contactos', 'ContactoController@store');
+//     $router->put('/contactos/{id}', 'ContactoController@update');
+//     $router->delete('/contactos/{id}', 'ContactoController@destroy');
+// });
+
