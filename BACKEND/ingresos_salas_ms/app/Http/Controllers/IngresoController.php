5d2c5e01354e8e6c2e8646865350733ccb201a7d
DsBm2468 <dsbenavides@uniboyaca.edu.co>
2024-05-29 16:57:29 -0500

    Intento de condicional de horario

--- BACKEND/ingresos_salas_ms/app/Http/Controllers/IngresoController.php
+++ BACKEND/ingresos_salas_ms/app/Http/Controllers/IngresoController.php
@@ -32,7 +32,7 @@ class IngresoController extends Controller{
         $ingreso->idResponsable = $datos['idResponsable'];
         $ingreso->idSala = $datos['idSala'];
         //CONDICION
-        if($ingreso->fechaIngreso = "Mond" OR "Tue" OR "Wed" Or "Thu" OR "Fri"){
+        /*if($ingreso->fechaIngreso = "Mond" OR "Tue" OR "Wed" Or "Thu" OR "Fri"){
             if($ingreso->horaIngreso = "7:00-20:50"){
                 echo "";
             }
@@ -41,7 +41,7 @@ class IngresoController extends Controller{
             if($ingreso->horaIngreso = "7:00-16:30"){
                 echo ""; 
             }
-        }
+        }*/
 
         $ingreso->save();
         $data = ['data' => $ingreso];
